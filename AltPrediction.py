71c82c097bf968fd7465c481b25399137a009877
Dawid Szczerba <48264678+DawidSzczerba@users.noreply.github.com>
2021-12-16 19:41:03 +0100

    Add RandomForestRegressor

--- AltPrediction.py
+++ AltPrediction.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[21]:
 
 
 import pandas as pd
@@ -10,8 +10,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LinearRegression, Lasso
-from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, RepeatedKFold
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, RepeatedKFold, StratifiedKFold
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
 
 
 # In[2]:
@@ -98,40 +99,104 @@ rskf = RepeatedStratifiedKFold(n_splits=5, n_repeats=2, random_state=44)
 # In[13]:
 
 
-kf = RepeatedKFold(n_splits=5, n_repeats=2, random_state=666)
+rskf.get_n_splits(X, y)
 
 
 # In[14]:
+
+
+kf = RepeatedKFold(n_splits=5, n_repeats=2, random_state=666)
+
+
+# In[15]:
+
+
+kf.get_n_splits(X, y)
+
+
+# In[16]:
 
 
 grid_cv_lr = GridSearchCV(Lasso(), param_grid, scoring='r2', cv=kf, return_train_score=True, verbose=1000)
 grid_cv_lr_fit = grid_cv_lr.fit(X_train, y_train)
 
 
-# In[15]:
+# In[17]:
 
 
 print(grid_cv_lr_fit.best_score_)
 print(grid_cv_lr_fit.best_params_)
 
 
-# In[16]:
+# In[18]:
 
 
 lasso_model = Lasso(alpha =  0.5)
 lasso_model.fit(X_train, y_train)
 
 
-# In[17]:
+# In[19]:
 
 
 Y_pred_train = lasso_model.predict(X_train)
-print("Accuracy R2 --> ", lasso_model.score(X_train, y_train) * 100)
+print("Accuracy R2 --> ", lasso_model.score(X_train, y_train))
 
 
-# In[18]:
+# In[20]:
 
 
 Y_pred_test = lasso_model.predict(X_test)
-print("Accuracy R2 --> ", lasso_model.score(X_test, y_test) * 100)
+print("Accuracy R2 --> ", lasso_model.score(X_test, y_test))
+
+
+# In[22]:
+
+
+param_grid_rfr={"n_estimators": [1, 5, 10, 20, 30, 40, 50, 100],
+            "max_features": ["auto", "sqrt", "log2"],
+            "min_samples_split": [2, 4, 8, 16],
+            "bootstrap": [True, False],
+}
+
+
+# In[23]:
+
+
+grid_RandomForestRegressor = GridSearchCV(RandomForestRegressor(), param_grid_rfr, scoring='r2', cv=kf, n_jobs=-1, return_train_score=True, verbose=1000)
+
+grid_RandomForestRegressor.fit(X_train, y_train)
+
+
+# In[24]:
+
+
+print(grid_RandomForestRegressor.best_score_)
+print(grid_RandomForestRegressor.best_params_)
+
+
+# In[26]:
+
+
+rfr_model = RandomForestRegressor(bootstrap =  False, max_features = 'log2', min_samples_split = 8, n_estimators = 40)
+rfr_model.fit(X_train, y_train)
+
+
+# In[34]:
+
+
+Y_pred_train_rfr = rfr_model.predict(X_train)
+print("Accuracy R2 --> ", rfr_model.score(X_train, y_train))
+
+
+# In[35]:
+
+
+Y_pred_test_rfr = rfr_model.predict(X_test)
+print("Accuracy R2 --> ", rfr_model.score(X_test, y_test))
+
+
+# In[ ]:
+
+
+
 
