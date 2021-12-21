e3978a3773de5a48dc3c3df6871e3588adc70f53
Dawid Szczerba <DawidSzczerba@protonmail.com>
2021-12-21 03:33:11 +0100

    Add requirements.txt

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -7,3 +7,7 @@ def prepare_data(file_path: str, output_file_path: str, column_0_name: str):
     finger_prints = data.iloc[1::2, ].reset_index(drop=True).drop('Unnamed: 0', axis=1)
     finger_prints.insert(0, column_0_name, data_values)
     finger_prints.to_csv(output_file_path, index=False)
+
+
+# prepare_data(file_path='../data/hepatotoxicity_ALT_MACCSFP_ready_set.csv',
+#              output_file_path='../data/alt_maccsfp_py.csv', column_0_name="ALT")
