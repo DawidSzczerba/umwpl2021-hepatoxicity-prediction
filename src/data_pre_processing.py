2ec81acb4fc98f9c37ccfe63518b504d55e8c0fd
Dawid Szczerba <DawidSzczerba@protonmail.com>
2022-01-06 17:32:11 +0100

    Change log to log1p for data pre processing

--- src/data_pre_processing.py
+++ src/data_pre_processing.py
@@ -10,7 +10,7 @@ def pre_process_data(file_path: str, output_file_path: str):
     data_train.reset_index(drop=True, inplace=True)
 
     # log the values of the main parameter
-    data_train[data_train.columns[0]] = np.log(data_train[data_train.columns[0]])
+    data_train[data_train.columns[0]] = np.log1p(data_train[data_train.columns[0]])
 
     # we are checking if there is no situation that in any column the minimum value == 1
     assert not any(data_train.iloc[:, 1:].min())
@@ -21,13 +21,12 @@ def pre_process_data(file_path: str, output_file_path: str):
     # let's check if the logarithmic values are correct
     test_data_train = pd.read_csv(file_path)
     assert int(data_train[data_train.columns[0]].values.sum()) == int(
-        np.log(test_data_train[test_data_train.columns[0]]).sum())
+        np.log1p(test_data_train[test_data_train.columns[0]]).sum())
 
     # save data after pre-processing to csv file
     data_train.to_csv(output_file_path, index=False)
 
+# file_path = "../data/pTD50_maccsfp.csv"
+# output_file_path = "../data/pTD50_maccsfp_after_preprocessing.csv"
 
-file_path = "../data/pTD50_maccsfp.csv"
-output_file_path = "../data/pTD50_maccsfp_after_preprocessing.csv"
-
-pre_process_data(file_path, output_file_path)
+# pre_process_data(file_path, output_file_path)
